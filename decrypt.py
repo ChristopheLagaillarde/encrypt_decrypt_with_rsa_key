0832d80b9550f85b468dbfdd12c2d7cd757add27
ChristopheLagaillarde <chrislag94@gmail.com>
2022-06-29 22:25:08 +0400

    first commit

---
+++ decrypt.py
@@ -0,0 +1,8 @@
+# Program : Decrypt files
+# Description : Allow to decrypt file encrypted with the RSA key
+# Date : 29/06/22
+# Author : Christophe Lagaillarde
+# Version : 1.0
+
+def decrypt():
+
---
+++ encrypt.py
@@ -0,0 +1,7 @@
+# Program : Encrypt files
+# Description : Allow to decrypt file encrypted with the RSA key
+# Date : 29/06/22
+# Author : Christophe Lagaillarde
+# Version : 1.0
+
+def encrypt():
